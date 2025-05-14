b4c1361a55080071b6a9cc6e0c7402d0734fbe05
evelyn villarreal <evelyn.villarreal245@gmail.com>
2025-05-14 12:12:20 -0300

    blueprintVistas,plantillas y archivos estaticos

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,7 +2,6 @@ import os
 
 from flask import Flask
 
-
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -29,4 +28,9 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
     return app
